eccaf589bee2ad98ad4727f8e85adf7f11214b39
hiiru <hiiru@rulez.ch>
2016-10-13 20:29:24 +0200

    case-sensitivy bug added

--- src/RestoreProblem/Program.cs
+++ src/RestoreProblem/Program.cs
@@ -14,7 +14,6 @@ namespace RestoreProblem
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
 
